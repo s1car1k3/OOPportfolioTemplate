d2668f2af725bafa6a737de8322f625b26967685
c3708602 <ak@>
2025-02-27 13:16:24 +0000

    Week 2 exercises

---
+++ WeeklyExercises/2DataAndExpressions/Week2.1/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("///////////////////" + "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" + "///////////////////");
+        System.out.println(" ");
+        System.out.println("Name            Lab     Bonus   Total");
+        System.out.println("----            ---     -----   -----");
+        System.out.println("Joe             43      7       50");
+        System.out.println("William         50      8       58");
+        System.out.println("Mary Sue        39      10      49");
+        System.out.println("Bob             52      16      68");
+        System.out.println("Thomas          35      7       42");
+    }
+}
