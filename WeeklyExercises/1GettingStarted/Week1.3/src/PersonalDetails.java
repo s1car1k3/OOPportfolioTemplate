f5db18a9856c054c4c8223615107275370458c54
c3708602 <ak@>
2025-02-27 13:40:50 +0000

    Week 1 exercises

---
+++ WeeklyExercises/1GettingStarted/Week1.3/src/PersonalDetails.java
@@ -0,0 +1,11 @@
+
+public class PersonalDetails {
+    public static void main(String[] args) {
+
+        System.out.println("Name: John");
+        System.out.println("Surname: Smith");
+        System.out.println("Age: 37");
+        System.out.println("Address: Oxford Street");
+        System.out.println("Phone number: 555-5555");
+    }
+}
